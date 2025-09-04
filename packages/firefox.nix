9713e0c0213d75e5d5b34fe9670e2b459c60dbe6
Konrad-hirschkorn <konrad.hirschkorn@gmail.com>
2025-09-04 12:04:48 +0200

    Refactor Firefox extension settings: replace placeholder for Improve YouTube with actual ID and remove Return YouTube Dislikes extension

--- packages/firefox.nix
+++ packages/firefox.nix
@@ -62,12 +62,11 @@ in {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
             installation_mode = "force_installed";
           };
-          # Enhancer for YouTube
-          "<IMPROVE_YOUTUBE_ID>" = {
-            installation_mode = "force_installed";
+          # Improve YouTube! (youtube-addon)
+          "{3c6bf0cc-3ae2-42fb-9993-0d33104fdcaf}" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-addon/latest.xpi";
+            installation_mode = "force_installed";
           };
-
           # User Agent Switcher and Manager:
           "{a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7}" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/user-agent-string-switcher/latest.xpi";
@@ -107,12 +106,6 @@ in {
           "restoreGoogleMaps@stephanmc.com" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/restore-google-maps-on-search/latest.xpi";
             installation_mode = "force_installed";
-          };
-          # Return YouTube Dislikes (two different extensions, in case one stops working)
-          "improvedtube@youtubetools.com" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-addon/latest.xpi";
-            installation_mode = "force_installed";
-            default_area = "menupanel";
           };
         };
         /*
